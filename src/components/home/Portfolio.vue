0b497e675c1b3b582065fb7dfce29c1955bcf132
Matt Briden <mattbridensoftware@gmail.com>
2021-08-11 12:22:22 -0400

    update portfolio for mobile

--- src/components/home/Portfolio.vue
+++ src/components/home/Portfolio.vue
@@ -2,9 +2,41 @@
   <div id="portfolio">
     <div class="wrapper">
     <h1>Featured Projects</h1>
-      <div class="featured-projects">
+      <div v-if="this.onMobile" class="featured-projects">
+        <div class="am-project" @click="toggleInfo('am-info')">
+          <div id="am-info" class="mobile-info">
+            <h2>Amenity Match</h2>
+            <h3>Spring Boot / Vue.js / PostgreSQL / AWS</h3>
+            <div class="view-site">
+              <a target="_blank" href="https://www.amenitymatch.com/">Learn More</a>
+            </div>
+          </div>
+          <img src="~@/assets/amenity-match-logo.svg" height="150px"/>
+        </div>
+        <div class="fg-project" @click="toggleInfo('fg-info')">
+          <div id="fg-info" class="mobile-info">
+            <h2>FileGenius</h2>
+            <h3>AWS Lambda Functions / Python / Terraform / React</h3>
+            <div class="view-site">
+              <a target="_blank" href="https://www.filegenius.com/">Learn More</a>
+            </div>
+          </div>
+          <img src="~@/assets/file-genius-logo.png" height="250px"/>
+        </div>
+        <div class="symbio-project" @click="toggleInfo('symbio-info')">
+          <div id="symbio-info" class="mobile-info">
+            <h2>Proofpoint</h2>
+            <h3>Ember.js / Spring Boot / AWS StepFunctions</h3>
+            <div class="view-site">
+              <a target="_blank" href="https://www.proofpoint.com/us">Learn More</a>
+            </div>
+          </div>
+          <img src="~@/assets/proofpoint-logo.png" height="100px"/>
+        </div>
+      </div>
+      <div v-else class="featured-projects">
         <div class="am-project">
-          <div id="am-info" class="info">
+          <div class="info">
             <h2>Amenity Match</h2>
             <h3>Spring Boot / Vue.js / PostgreSQL / AWS</h3>
             <div class="view-site">
@@ -14,7 +46,7 @@
           <img src="~@/assets/amenity-match-logo.svg" height="150px"/>
         </div>
         <div class="fg-project">
-          <div id="fg-info" class="info">
+          <div class="info">
             <h2>FileGenius</h2>
             <h3>AWS Lambda Functions / Python / Terraform / React</h3>
             <div class="view-site">
@@ -24,7 +56,7 @@
           <img src="~@/assets/file-genius-logo.png" height="250px"/>
         </div>
         <div class="symbio-project">
-          <div id="symbio-info" class="info">
+          <div class="info">
             <h2>Proofpoint</h2>
             <h3>Ember.js / Spring Boot / AWS StepFunctions</h3>
             <div class="view-site">
@@ -41,7 +73,32 @@
 
 <script>
 export default {
-  name: 'Portfolio'
+  name: 'Portfolio',
+  data() {
+    return {
+      onMobile: false
+    }
+  },
+  created() {
+    this.updateSize();
+  },
+  methods: {
+    updateSize() {
+      this.onMobile = window.innerWidth <= 900;
+    },
+    toggleInfo(el) {
+      let element = document.getElementById(el);
+      if (element.style.display === 'block') {
+        element.style.display = 'none';
+      } else {
+        element.style.display = 'block';
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('load', this.updateSize);
+    window.addEventListener('resize', this.updateSize);
+  }
 }
 </script>
 
@@ -113,7 +170,7 @@ export default {
   opacity: 0.8;
 }
 
-.info .view-site {
+.info .view-site, .mobile-info .view-site {
   padding-top: 35px;
 }
 .info .view-site a {
@@ -125,21 +182,70 @@ export default {
   padding-top: 10px;
   padding-bottom: 10px;
 }
+
+.mobile-info .view-site a {
+  text-decoration: none;
+  color: white;
+  border: thick solid white;
+  font-weight: bold;
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+
+.mobile-info {
+  background-color: rgba(0, 0, 0, 0.8);
+  border-radius: 25px;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  color: white;
+  display: none;
+
+  -webkit-animation: fadein 1s; /* Safari, Chrome and Opera > 12.1 */
+  -moz-animation: fadein 1s; /* Firefox < 16 */
+  -ms-animation: fadein 1s; /* Internet Explorer */
+  -o-animation: fadein 1s; /* Opera < 12.1 */
+  animation: fadein 1s;
+}
+
 .info .view-site a:hover {
   border: thick solid white;
   font-weight: bold;
 }
-@media screen and (max-width: 600px) {
+
+@keyframes fadein {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
+/* Firefox < 16 */
+@-moz-keyframes fadein {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
+/* Safari, Chrome and Opera > 12.1 */
+@-webkit-keyframes fadein {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
+/* Internet Explorer */
+@-ms-keyframes fadein {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
+/* Opera < 12.1 */
+@-o-keyframes fadein {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
+@media screen and (max-width: 900px) {
   .fg-project, .symbio-project, .am-project {
     grid-column: span 12;
   }
   .wrapper {
     padding: 15px;
     margin: 15px;
-  }
-
-  .info:hover {
-    opacity: 0;
   }
 }
 </style>
